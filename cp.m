d6e4f8afdb9ffdf64e3f6320650d174c609f527a
Gregor Strugala <gregor.strugala@gmail.com>
2016-11-19 14:40:47 +0100

    added cd and deaerator functions

---
+++ cp.m
@@ -0,0 +1,13 @@
+function [ Cp ] = cp(T,A)
+%CP computes the heat capacity of species A for a certain temperature.
+%   CP(T,A) returns the value of the heat capacity of species A (A must be
+%   a string) at temperature T, eg cp(600,'CO2').
+
+if T < 300
+    dT = 300-T;
+    Cp = 2*janaf('c',A,300) - janaf('c',A,300+dT);
+else
+    Cp = janaf('c',A,T);
+end
+end
+
---
+++ deaerator.m
@@ -0,0 +1,7 @@
+function [ stateO ] = deaerator( stateI, stateII )
+%DEAERATOR computes the state variation after a de-gazing and a heat
+%exchange.
+
+
+end
+
